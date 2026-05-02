20cf3f08be4766098e6f7d8179c163fe67f28cef
Nanciee722 <2781340243@qq.com>
2026-05-02 15:05:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def img2text(url):
     return text
 
 # Main part
-st.set_page_config(page_title="Your Image to Audio Story", page_icon="🛬"
+st.set_page_config(page_title="Your Image to Audio Story", page_icon="🛬")
 
 
 <g transform="matrix(0.71 0 0 0.71 12 12)" >
